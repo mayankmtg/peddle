ad7c7d193bcbfc88cb9d05b286c12036b875de9e
mayankmtg <mayank15056@iiitd.ac.in>
2017-05-27 18:58:10 +0530

    login and cart left

--- Home/urls.py
+++ Home/urls.py
@@ -6,5 +6,8 @@ app_name='Home'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^product/(?P<cat_name>\w+)/$', views.product, name='product'),
     url(r'^cat=(?P<cat_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Catagory, Offer
+from .models import Catagory, Offer, Product
 from django.shortcuts import render
 import operator
 
@@ -21,3 +21,23 @@ def index(request):
 def detail(request, cat_id):
 	catagory=get_object_or_404(Catagory, pk=cat_id)
 	return render(request, 'Home/detail.html', { 'catagory' : catagory })
+
+def about(request):
+	context={}
+	return render(request, 'Home/about.html', context)
+def contact(request):
+	context={}
+	return render(request, 'Home/contact.html', context)
+def product(request, cat_name):
+	all_cats=Catagory.objects.all()
+	# print(cat_name)
+	if cat_name == "all":
+		prods=Product.objects.all()
+	else:
+		prods=Product.objects.filter(catagory__name=cat_name)
+
+	context={
+		'all_cats' : all_cats,
+		'prods' : prods,
+	}
+	return render(request, 'Home/product.html', context)
